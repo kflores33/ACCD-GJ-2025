245a01f6e841e5f4f76cce24ba13f1d2e6b0fcdd
Mason <conrad.milliken@gmail.com>
2025-03-22 15:05:31 -0700

    Fixed Enemies

--- ACCDGJSpring2025/Assets/Conrad/Scripts/C_GameManagerMockup.cs
+++ ACCDGJSpring2025/Assets/Conrad/Scripts/C_GameManagerMockup.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class C_GameManagerMockup : MonoBehaviour
@@ -7,12 +8,15 @@ public class C_GameManagerMockup : MonoBehaviour
     public float enemySpeed;
     public float numberOfKilledEnemies;
     public bool isWeak;
+    public bool canSpawn = true;
+    public float spawnInterval;
+    public C_EnemySpawnScript spawnerScript;
 
     public void Update()
     {
         if (isWeak)
         {
-            arrowSpeed = arrowSpeed + Time.deltaTime/2;
+            arrowSpeed = arrowSpeed + Time.deltaTime / 2;
             if (Input.GetKeyDown(KeyCode.H))
             {
                 isWeak = false;
@@ -20,11 +24,36 @@ public class C_GameManagerMockup : MonoBehaviour
         }
         else
         {
-            enemySpeed = enemySpeed + Time.deltaTime/2;
+            enemySpeed = enemySpeed + Time.deltaTime / 2;
             if (Input.GetKeyDown(KeyCode.H))
             {
                 isWeak = true;
             }
+        }
+        if (canSpawn)
+        {
+            StartCoroutine(thingSpawn());
+
+            if (spawnInterval > 0.1f)
+            {
+                spawnInterval = spawnInterval - Time.deltaTime;
+            }
+        }
+
+        IEnumerator thingSpawn()
+        {
+            canSpawn = false;
+            if (isWeak)
+            {
+                spawnerScript.SpawnArrow();
+            }
+            else
+            {
+                spawnerScript.SpawnEnemy();
+            }
+            yield return new WaitForSeconds(spawnInterval);
+            canSpawn = true;
+            StopCoroutine(thingSpawn());
         }
     }
 }
