e41537719699563a3884b81159a83971d6ae3d12
papercdisc <kflores3@inside.artcenter.edu>
2025-03-23 02:18:35 -0700

    visual updates and title screen functionality

---
+++ ACCDGJSpring2025/Assets/Kay/Scripts/K_SceneManager.cs
@@ -0,0 +1,25 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class K_SceneManager : MonoBehaviour
+{
+    public enum TargetScene
+    {
+        Title,
+        Main
+    }
+    public TargetScene targetScene;
+
+    public void ChangeScene()
+    {
+        if (targetScene == TargetScene.Title)
+        {
+            SceneManager.LoadScene("TitleScene");
+        }
+        else if (targetScene == TargetScene.Main)
+        {
+            SceneManager.LoadScene("Conrad_TestScene");
+        }
+    }
+}
