b3fd4afae3c15bfd89369f2bfefcef9c9da7bb2b
papercdisc <kflores3@inside.artcenter.edu>
2025-03-24 00:35:03 -0700

    ok actually the final build im fr this time

--- ACCDGJSpring2025/Assets/Kay/Scripts/K_WizardBehavior.cs
+++ ACCDGJSpring2025/Assets/Kay/Scripts/K_WizardBehavior.cs
@@ -98,7 +98,10 @@ public class K_WizardBehavior : MonoBehaviour
         {
             if (_mustDelay) { _delayCoroutine = StartCoroutine(Delay(wizardStats.ManaDepletionDelay)); } // if can delay, wait for delay time
 
-            if (_canDepleteRegen) currentMana -= (wizardStats.ManaDepletionRate * (_gameManager.spawnSpeedMultiplier/4)) * Time.deltaTime; // deplete mana over time
+            float depletionRate = wizardStats.ManaDepletionRate * (_gameManager.spawnSpeedMultiplier / 4);
+            if(depletionRate >4) { depletionRate = 4; } // cap the depletion rate at 4
+
+            if (_canDepleteRegen) currentMana -= depletionRate * Time.deltaTime; // deplete mana over time
         }
         else currentMana = 0;
 
